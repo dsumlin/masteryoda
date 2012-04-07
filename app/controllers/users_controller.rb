aac39cd2273e685d37fff3c1cc1cd5eff8716c31
dsumlin <webmaster@dennissumlin.com>
2012-04-07 18:38:08 -0400

    added pics

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
     
     
     def index
-        @users = User.paginate(:page => params[:page], :per_page => 5)
+        @users = User.paginate(:page => params[:page], :per_page => 1)
         @title = "MEMBERS"
         end
     
